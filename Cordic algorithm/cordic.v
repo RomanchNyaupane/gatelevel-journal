15eb9855f10ee88645764add47553c9d0cf53258
Romanch Neupane <96409863+RomanchNyaupane@users.noreply.github.com>
2025-08-02 21:07:46 +0545

    Update cordic.v

--- Cordic algorithm/cordic.v
+++ Cordic algorithm/cordic.v
@@ -1,66 +1,58 @@
-        module cordic (
-            input wire [15:0] angle,
-            input wire clk, reset,
-            output reg [15:0] x, y
-        );
-        reg [15:0] x_reg, y_reg, z_reg;
-        reg [15:0] x_reg_1, y_reg_1, z_reg_1;
-        reg [2:0] iteration;
-        reg comparator;
-        
-        reg [15:0] tan_table;
-        
-        
-        always @(posedge clk) begin
-            if (reset) begin
-                x_reg <= 16'd256;
-                y_reg <= 16'b0; x_reg_1<= 16'b0; y_reg_1<= 16'b0;
-                z_reg_1 <= 16'b0;
-                z_reg <= angle;
-                iteration <= 3'b000;
-            end else begin  
-            
+module cordic (
+    input wire signed [15:0] angle,
+    input wire clk, reset,
+    output reg signed [15:0] x, y
+);
 
-                
-                if(!comparator) begin
-                    x_reg <= x_reg - (y_reg >> iteration);
-                    y_reg <= y_reg + (x_reg >> iteration);
-                    z_reg <= z_reg - (tan_table);
-                end else begin
-                    x_reg <= x_reg + (y_reg >> iteration);
-                    y_reg <= y_reg - (x_reg >> iteration);
-                    z_reg <= z_reg + (tan_table);
-                end
-        
-                if(iteration < 3'd7) 
-                    iteration <= iteration + 1;
-                 else 
-                    iteration <= 3'd0;
-                    x <= x_reg ;
-                    y <= y_reg ;
-                end
-                
+parameter STAGES = 6;
+reg signed [15:0] x_reg [0:STAGES];
+reg signed [15:0] y_reg [0:STAGES];
+reg signed [15:0] z_reg [0:STAGES];
+
+reg signed [15:0] atan_table [0:STAGES];
+initial begin
+    atan_table[0] = 16'sd11520;//45.0°
+    atan_table[1] = 16'sd6805;//26.565°
+    atan_table[2] = 16'sd3593;//14.036°
+    atan_table[3] = 16'sd1824;//7.125°
+    atan_table[4] = 16'sd915; //3.576°
+    atan_table[5] = 16'sd458; //1.790°
+    atan_table[6] = 16'sd229; //0.895°
+end
+
+always @(posedge clk) begin
+    if (reset) begin
+            x_reg[0] <= 16'sd0;
+            y_reg[0] <= 16'sd0;
+            z_reg[0] <= 16'sd0;
+    end else begin
+        x_reg[0] <= 16'sd256; 
+        y_reg[0] <= 16'sd0;
+        z_reg[0] <= angle;
+   end
+        x <= x_reg[6];
+        y <= y_reg[6];
+end
+
+genvar i;
+generate for (i = 0; i < STAGES; i = i + 1) begin : cordic_stages
+    always @(posedge clk) begin
+        if (reset) begin
+            x_reg[i+1] <= 16'sd0;
+            y_reg[i+1] <= 16'sd0;
+            z_reg[i+1] <= 16'sd0;
+        end else begin
+            if (z_reg[i][15]) begin  
+                x_reg[i+1] <= x_reg[i] + (y_reg[i] >>> i);
+                y_reg[i+1] <= y_reg[i] - (x_reg[i] >>> i);
+                z_reg[i+1] <= z_reg[i] + atan_table[i];
+            end else begin           
+                x_reg[i+1] <= x_reg[i] - (y_reg[i] >>> i);
+                y_reg[i+1] <= y_reg[i] + (x_reg[i] >>> i);
+                z_reg[i+1] <= z_reg[i] - atan_table[i];
+            end
         end
-        
-        always @(*) begin
-        
-             if (z_reg[15:14] == 2'b11)
-                comparator <= 1'b1;
-            else
-                comparator <= 1'b0;
-                
-            case (iteration)
-                3'd0: tan_table = 16'd11520; // 45.0000°
-                3'd1: tan_table = 16'd6805;  // 26.5651°
-                3'd2: tan_table = 16'd3593;  // 14.0362°
-                3'd3: tan_table = 16'd1824;  // 7.1250°
-                3'd4: tan_table = 16'd915;   // 3.5763°
-                3'd5: tan_table = 16'd458;   // 1.7899°
-                3'd6: tan_table = 16'd229;   // 0.8952°
-                default: tan_table = 16'd0;
-            endcase
-        end
-        
-        
-        endmodule
-        
+    end
+end endgenerate
+
+endmodule
