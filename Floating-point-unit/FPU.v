15539248aed2f78947b07f60527278e6064ccce8
Romanch <romanchneupane53@gmail.com>
2025-08-08 22:35:03 +0545

    added addition and subtraction

--- Floating-point-unit/FPU.v
+++ Floating-point-unit/FPU.v
@@ -1,6 +1,6 @@
 module FPU(
     input wire [31:0] FP_in1, FP_in2,
-    input wire calc_mode,
+    input wire calc_mode,           //  // 0 for addition, 1 for subtraction
     input wire [1:0] round_mode,
     input wire clk, reset,
 
@@ -39,7 +39,10 @@ reg [23:0] S_01_norm_mantissa1, S_01_norm_mantissa2;
 reg [1:0] S_12_round_mode;
 reg S_12_calc_mode, S_12_is_subnormal1, S_12_is_subnormal2;
 reg [23:0] S_12_norm_mantissa1, S_12_norm_mantissa2;
+reg S_12_FP_1_sign, S_12_FP_2_sign; // Sign bits of the inputs
 
+reg [25:0] S_23_FP_result;  //26th bit is 1 if sign of result is negative
+reg 
 
 
 
@@ -53,14 +56,40 @@ always @ (posedge clk) begin
     S_01_FP_in2 <= FP_in2;
 
 //stage 1-2
-    S_12_is_subnormal1 <= S_01_is_subnormal1;
-    S_12_is_subnormal2 <= S_01_is_subnormal2;
+    //S_12_is_subnormal1 <= S_01_is_subnormal1;
+    //S_12_is_subnormal2 <= S_01_is_subnormal2;
+    S_12_FP_1_sign <= S_01_FP_in1[31];
+    S_12_FP_2_sign <= S_01_FP_in2[31];  
     S_12_round_mode <= S_01_round_mode;
     S_12_calc_mode <= S_01_calc_mode;
     S_12_norm_mantissa1 <= normalized_mantissa1;
     S_12_norm_mantissa2 <= normalized_mantissa2;
 
 //stage 2-3
-    
+    //subtraction
+    case ({S_12_calc_mode,S_12_FP_1_sign, S_12_FP_2_sign})
+        3'b100 : S_23_FP_result <= {1'b0, S_12_norm_mantissa1} - {1'b0, S_12_norm_mantissa2}; //both are positive
+        3'b101 : S_23_FP_result <= {1'b0, S_12_norm_mantissa1} + {1'b0, S_12_norm_mantissa2}; //FP_in1 is positive, FP_in2 is negative
+        3'b110 : begin
+                S_23_FP_result <= {1'b0, S_12_norm_mantissa1} + {1'b0, S_12_norm_mantissa2}; //FP_in1 is negative, FP_in2 is positive
+                S_23_FP_result[25] <= 1'b1; // Result is negative
+            end
+        3'b111 : S_23_FP_result <= {1'b0, S_12_norm_mantissa2} - {1'b0, S_12_norm_mantissa1}; //both are negative
+    //ad3ition
+        3'b000 : S_23_FP_result <= {1'b0, S_12_norm_mantissa1} + {1'b0, S_12_norm_mantissa2}; //both are positive
+        3'b001 : S_23_FP_result <= {1'b0, S_12_norm_mantissa1} - {1'b0, S_12_norm_mantissa2}; //FP_in1 is positive, FP_in2 is negative
+        3'b010 : S_23_FP_result <= {1'b0, S_12_norm_mantissa2} - {1'b0, S_12_norm_mantissa1}; //FP_in1 is negative, FP_in2 is positive
+        3'b011 : begin
+                S_23_FP_result <= {1'b0, S_12_norm_mantissa2} + {1'b0, S_12_norm_mantissa1}; //both are negative
+                S_23_FP_result[25] <= 1'b1; // Result is negative
+        end
+
+        default: begin
+            S_23_FP_result <= 26'b0; // Default case, should not happen
+        end
+    endcase
+
+
+
 end
 endmodule
