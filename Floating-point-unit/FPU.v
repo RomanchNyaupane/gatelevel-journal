7e067e63f01bf10cd93aa398468fa3795b2ff3df
Romanch <romanchneupane53@gmail.com>
2025-08-08 21:34:03 +0545

    added prenormalization

--- Floating-point-unit/FPU.v
+++ Floating-point-unit/FPU.v
@@ -7,6 +7,7 @@ module FPU(
     output reg [31:0] FP_result,
 );
 wire is_subnormal1, is_subnormal2;
+wire [23:0] normalized_mantissa1, normalized_mantissa2;
 
 .subnormal_detection subnorm_in1(
     .FP_in(FP_in1),
@@ -18,20 +19,48 @@ wire is_subnormal1, is_subnormal2;
     .is_subnormal(is_subnormal2),
     .clk(clk)
 );
+.prenormalization prenorm(
+    .FP_in1(FP_in1),
+    .FP_in2(FP_in2),
+    .calc_mode(calc_mode),
+    .clk(clk),
+    .subnormal_status({is_subnormal1, is_subnormal2}),
+    .FP_norm1(normalized_mantissa1),// Assuming prenormalization outputs 24 bits 
+    .FP_norm2(normalized_mantissa2)// Assuming prenormalization outputs 24 bits 
+);
 
-reg [31:0] S_01_FP_in1, S_01_FP_in2;
+
+
 reg [1:0] S_01_round_mode;
-reg S_01_round_mode;
-reg S_01_is_subnormal1, S_01_is_subnormal2;
+reg S_01_calc_mode, S_01_is_subnormal1, S_01_is_subnormal2;
+reg [31:0] S_01_FP_in1, S_01_FP_in2;
+reg [23:0] S_01_norm_mantissa1, S_01_norm_mantissa2;
+
+reg [1:0] S_12_round_mode;
+reg S_12_calc_mode, S_12_is_subnormal1, S_12_is_subnormal2;
+reg [23:0] S_12_norm_mantissa1, S_12_norm_mantissa2;
+
+
+
 
 always @ (posedge clk) begin
-//stage 0
+//stage 0-1
     S_01_is_subnormal1 <= is_subnormal1;
     S_01_is_subnormal2 <= is_subnormal2;
-    S_01_FP_in1 <= FP_in1;
-    S_01_FP_in2 <= FP_in2;
     S_01_round_mode <= round_mode;
     S_01_calc_mode <= calc_mode;
+    S_01_FP_in1 <= FP_in1;
+    S_01_FP_in2 <= FP_in2;
 
+//stage 1-2
+    S_12_is_subnormal1 <= S_01_is_subnormal1;
+    S_12_is_subnormal2 <= S_01_is_subnormal2;
+    S_12_round_mode <= S_01_round_mode;
+    S_12_calc_mode <= S_01_calc_mode;
+    S_12_norm_mantissa1 <= normalized_mantissa1;
+    S_12_norm_mantissa2 <= normalized_mantissa2;
+
+//stage 2-3
+    
 end
 endmodule
--- Floating-point-unit/prenormalization.v
+++ Floating-point-unit/prenormalization.v
@@ -4,29 +4,44 @@ module prenormalization(
     input wire clk,
     input wire [1:0] subnormal_status,
 
-    output reg [31:0] FP_result
+    output reg [23:0] FP_norm1, FP_norm2
 );
-reg [22:0] shifted_mantissa1, shifted_mantissa2;
 wire compare_exponents; // 1 if FP_in1 has a smaller exponent, 0 if FP_in2 has a smaller exponent
 assign compare_exponents = (FP_in1[30:23] < FP_in2[30:23]) ? 1'b1 : 1'b0;
 always @(posedge clk) begin
     case(subnormal_status)
         2'b00: begin    //both inputs are normal
             if(compare_exponents) begin
-                shifted_mantissa1 <= (FP_in1[30:23] >> (FP_in1[30:23] - FP_in2[30:23]));
-                shifted_mantissa2 <= FP_in2[30:23]
+                FP_norm1[22:0] <= (FP_in1[22:0] >> (FP_in1[30:23] - FP_in2[30:23]));
+                FP_norm2[22:0] <= FP_in2[22:0];
             end else begin
-                shifted_mantissa2 <= (FP_in2[30:23] >> (FP_in2[30:23] - FP_in1[30:23]));
-                shifted_mantissa1 <= FP_in1[30:23];
+                FP_norm2[22:0] <= (FP_in2[22:0] >> (FP_in2[30:23] - FP_in1[30:23]));
+                FP_norm1[22:0] <= FP_in1[22:0];
             end
+            FP_norm1[23] <= 1'b1;
+            FP_norm2[23] <= 1'b1;
         end
-        2'b01, 2'b10, 2'b11: begin    // one input or both input are subnormal
-            shifted_mantissa1 <= FP_in1[22:0];
-            shifted_mantissa2 <= FP_in2[22:0];
+        2'b01: begin    // only FP_in1 is subnormal
+            FP_norm1 <= FP_in1[22:0];
+            FP_norm2 <= (FP_in2[22:0] >> (FP_in2[30:23] - FP_in1[30:23]));
+            FP_norm1[23] <= 1'b0;
+            FP_norm2[23] <= 1'b1;
+        end
+        2'b10: begin    // only FP_in2 is subnormal
+            FP_norm1 <= FP_in1[22:0];
+            FP_norm2 <= FP_in2[22:0];
+            FP_norm1[23] <= 1'b1;
+            FP_norm2[23] <= 1'b0;
+        end
+        2'b11: begin    // both inputs are subnormal
+            FP_norm1 <= FP_in1[22:0];
+            FP_norm2 <= FP_in2[22:0];
+            FP_norm1[23] <= 1'b0;
+            FP_norm2[23] <= 1'b0;
         end
         default: begin
-            shifted_mantissa1 <= 23'b0;
-            shifted_mantissa2 <= 23'b0;
+            FP_norm1 <= 23'b0;
+            FP_norm2 <= 23'b0;
         end
     endcase
 end
